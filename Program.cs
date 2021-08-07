10712b7c61270218f89e6282b39c64ca8ed80c43
Ilya <ronny0000@yandex.ru>
2021-08-07 15:11:33 +0300

    Lesson6.1

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace HomeWorkGeekBrains
 {
@@ -10,6 +11,123 @@ namespace HomeWorkGeekBrains
     {
         static void Main(string[] args)
         {
+            //GetProcessesList();
+            //while (true)
+            //{
+            //    Console.WriteLine(" Введите:");
+            //    Console.WriteLine(" Название процесса или его ID, чтобы завершить процесс или End, чтобы выйти из программы");
+            //    Console.WriteLine(" 'Обновить', чтобы обновить список процессов");
+            //    Console.WriteLine(" 'Выйти', чтобы выйти из программы");
+            //    string nameOrId = Console.ReadLine();                                            
+            //    if (nameOrId == "Выйти" || nameOrId == "выйти")
+            //    {
+            //        break;
+            //    } else if (nameOrId == "Обновить" || nameOrId == "обновить")
+            //    {
+            //        Console.Clear();
+            //        GetProcessesList();
+            //    } else
+            //    {
+            //        KillProcess(nameOrId);
+            //    }
+            //}
+            string[,] massa = new string[5, 5];
+            try
+            {
+                SumOfMassive mass = new SumOfMassive { mas = massa};
+            }
+            catch(MyArraySizeException ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+            
+        }
+           
+
+
+
+
+
+        static void GetProcessesList()
+        {
+            Process[] list = Process.GetProcesses();
+            Console.WriteLine("  ID      Имя процесса                       Использование памяти");
+            Console.WriteLine();
+            for (int i = 0; i < list.Length; i++)
+            {
+                Console.SetCursorPosition(0, i + 2);
+                Console.Write("  " + list[i].Id);
+                Console.SetCursorPosition(10, i + 2);
+                Console.Write(" " + list[i].ProcessName);
+                Console.SetCursorPosition(45, i + 2);
+                Console.Write($" {list[i].WorkingSet64 / (1024 * 1024):f2}");
+                Console.SetCursorPosition(53, i + 2);
+                Console.WriteLine(" МБ ");
+            }
+            Console.WriteLine();
+        }
+        static void KillProcess(string procname)
+        {
+            Process[] list = Process.GetProcesses();
+            bool flag = true;                                    //флаг проверки корректности введенных данных
+            if (IsID(procname))                                  //Проверяем ввели ID или же имя процесса
+            {
+                
+                try                                             //если ID, то обрабатываем исключение, если ввели слишком длинное число за пределами int
+                {
+                    int id = Convert.ToInt32(procname);
+                    for (int i = 0; i < list.Length; i++)        //находим наш процесс по ID и убиваем. снимаем флаг
+                    {
+                        if (list[i].Id == id)                            
+                        {
+                            list[i].Kill();
+                            Console.WriteLine($"Процесс c ID '{list[i].Id}' убит");
+                            Console.WriteLine();
+                            flag = false;
+                        }
+                    }
+                    if (flag)                                            //обрабатываем флаг
+                    {
+                        Console.WriteLine("Ошибка! Данный процесс не найден!");
+                        Console.WriteLine();
+                    }
+                }
+                catch                                                  
+                {
+                    Console.WriteLine("Ошибка! Некорректный ввод!");
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                for (int i = 0; i < list.Length; i++)                 //если ввели имя, то сразу ищем по имени среди всех процессов и меняем флаг, если нашли
+                {
+                    if (list[i].ProcessName == procname)
+                    {
+                        list[i].Kill();
+                        Console.WriteLine($"Процесс '{list[i].ProcessName}' убит");
+                        Console.WriteLine();
+                        flag = false;
+                    }
+                }
+                if (flag)
+                {
+                    Console.WriteLine("Ошибка! Данный процесс не найден!");
+                    Console.WriteLine();
+                }
+            }
+
+        }
+        static bool IsID(string source)                   //проверяем что ввел пользователь - текст или число (ID)
+        {
+            for (int i = 0; i < source.Length; i++)
+            {
+                if (source[i] < '0' || source[i] > '9')
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
---
+++ SumOfMassive.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkGeekBrains
+{
+    class SumOfMassive
+    {
+        private string[,] masiv;
+        public string[,] mas
+        {
+            get { return masiv; }
+            set
+            {
+                if (mas.GetLength(0) != 4 || mas.GetLength(1) != 4)
+                {
+                    throw new MyArraySizeException("Неверная размерность массива, должен быть 4 на 4");
+                }
+                else
+                {
+                    masiv = mas;
+                } 
+                    
+            }
+        }
+        public int SumOfMassivee(string[,] mas)
+        {
+            int sum = 0;
+            for (int i = 0; i < mas.GetLength(0); i++)
+            {
+                for (int j = 0; j < mas.GetLength(1); j++)
+                {
+                    int value = Convert.ToInt32(mas[i, j]);
+                    sum += value;
+                }
+
+            }
+            return sum;
+        }
+    }
+}
