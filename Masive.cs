5eb9e82bc56f0f59b6f255bae587e1e7f72c6947
Ilya <ronny0000@yandex.ru>
2021-08-15 21:21:54 +0300

    Lesson8

---
+++ Masive.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkGeekBrains
+{
+    class Masive
+    {
+        public static int[,] CreateMasive(int y, int x)
+        {
+            int[,] mas = new int[y, x];
+           
+            return mas;
+        }
+        public static void WriteMas(int [,] mas)
+        {
+            for (int i = 0; i < mas.GetLength(0); i++)
+            {
+                for (int j = 0; j < mas.GetLength(1); j++)
+                {
+                    Console.Write(mas[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        public static int CountLine(int [,] mas, bool nextORback, int yCurrent, int count)
+        {
+            int i = yCurrent;
+            if (nextORback)                                              //выбираем куда нам идти заполнять, вперед или назад по индексам
+            {
+                    for (int j = 0; j < mas.GetLength(1); j++)              //заполняем в заданном направлении, пока есть незаполненные индексы
+                    {
+                        if ( mas[i,j]==0)
+                        {
+                            mas[i, j] = count;
+                            count++;                                        //если поставили значение, то увеливаем счётчик и возвращаем его в конце
+                        }
+                    }
+            } else
+            {
+                for (int j = mas.GetLength(1)-1; j >= 0 ; j--)
+                {
+                    if (mas[i, j] == 0)
+                    {
+                        mas[i, j] = count;
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+        public static int CountStolb(int[,] mas, bool nextORback, int xCurrent, int count)
+        {
+            int j = xCurrent;
+            if (nextORback)                                  
+            {
+                for (int i = 0; i < mas.GetLength(0); i++)
+                {
+                    if (mas[i, j] == 0)
+                    {
+                        mas[i, j] = count;
+                        count++;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = mas.GetLength(0)-1; i >= 0; i--)
+                {
+                    if (mas[i, j] == 0)
+                    {
+                        mas[i, j] = count;
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+        public static bool IsFull(int[,] mas)
+        {
+            for (int i = 0; i < mas.GetLength(0); i++)
+            {
+                for (int j = 0; j < mas.GetLength(1); j++)
+                {
+                    if (mas[i,j]==0)
+                    {
+                        return false;
+                    }
+                }
+
+            }
+            return true;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,35 @@ namespace HomeWorkGeekBrains
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите размеры массива");
+            Console.Write("Количество строк: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Количество столбцов: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int [,] mas = Masive.CreateMasive(y,x);
+            int count = 1;
+            int lineright = 0;
+            int lineleft = mas.GetLength(0)-1;
+            int columndown = mas.GetLength(1)-1;
+            int columnup = 0;
+            do                                                           //заполняем массив по кругу, увеливая или уменьшая индексы строк и столбцов
+            {
+                count = Masive.CountLine(mas, true, lineright, count);
+                lineright++;
+                count = Masive.CountStolb(mas, true, columndown, count);
+                columndown--;
+                count = Masive.CountLine(mas, false, lineleft, count);
+                lineleft--;
+                count = Masive.CountStolb(mas, false, columnup, count);
+                columnup++;
+                if (Masive.IsFull(mas))              //если массив заполнен - выходим из цикла
+                {
+                    break;
+                }
+            } while (true);
+            Console.WriteLine("Ваш массив, заполненный по спирали: ");
+            Masive.WriteMas(mas);
         }
+       
     }
 }
