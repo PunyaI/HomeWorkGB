4db0d0b57b89c8c2228d96db8c9d776096d00620
Ilya <ronny0000@yandex.ru>
2021-08-26 15:20:52 +0300

    Algoritm.Lesson1

---
+++ Fibonacci.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkGeekBrains
+{
+    class Fibonacci
+    {
+        public static long count1 = 0;
+        public static long count2 = 0;
+        public static long RecursiveFibo(int n)
+        {
+            count1++;
+            if (n == 0 || n == 1)
+            {
+                return n;
+            } 
+            else
+            {
+                return RecursiveFibo(n - 1) + RecursiveFibo(n - 2);
+            }
+        }
+
+        public static long Fibo(int n)
+        {
+            int []numbers = new int[n];
+            numbers[0] = 1;
+            numbers[1] = 1;
+            for (int i = 2; i < numbers.Length; i++)
+            {
+                numbers[i] = numbers[i - 1] + numbers[i - 2];
+                count2++;
+            }
+            return numbers[n-1];
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace HomeWorkGeekBrains
 {
@@ -10,6 +7,116 @@ namespace HomeWorkGeekBrains
     {
         static void Main(string[] args)
         {
+            //Задание №1
+            var testcase1 = new TestCase()
+            {
+                input = "137",
+                expected = true,
+                expectedException = null
+            };
+            var testcase2 = new TestCase()
+            {
+                input = "47",
+                expected = true,
+                expectedException = null
+            };
+            var testcase3 = new TestCase()
+            {
+                input = "49",
+                expected = false,
+                expectedException = null
+            };
+            Test(testcase1);
+            Test(testcase2);
+            Test(testcase3);
+            Console.WriteLine();
+
+            //Задание №2
+            //Асимптотическая сложность функции из примера - O(N^3)
+
+            //Задание №3
+            int num = 5;
+            Stopwatch time = new Stopwatch();
+            for (int i = num; i < 50; i+=10)
+            {
+                Fibonacci.count1 = 0;
+                Fibonacci.count2 = 0;
+                time.Start();
+                Console.WriteLine($"{i} число Фибонаначи: {Fibonacci.RecursiveFibo(i)}");
+                time.Stop();
+                Console.WriteLine($"Рекурсивное вычисление {i} числа Фибонначи заняло: {time.ElapsedMilliseconds} милисекунд и {Fibonacci.count1} итераций");
+                time.Reset();
+                time.Start();
+                Console.WriteLine($"{i} число Фибонаначи: {Fibonacci.Fibo(i)}");
+                time.Stop();
+                Console.WriteLine($"Цикличное вычисление {i} числа Фибонначи заняло: {time.ElapsedMilliseconds} милисекунд и {Fibonacci.count2} итераций");
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+            
+        }
+
+        public class TestCase
+        {
+            public string input { get; set; }
+            public bool expected { get; set; }
+            public Exception expectedException { get; set; }
+        }
+
+        static void Test(TestCase testCase)
+        {
+            try
+            {
+                var actual = IsSimpleNumber(testCase.input);
+                if (actual == testCase.expected)
+                {
+                    Console.WriteLine("VALID TEST");
+                }
+                else
+                {
+                    Console.WriteLine("INVALID TEST");
+                }
+            }
+            catch (Exception e)
+            {
+                if (testCase.expectedException != null)
+                {
+                    Console.WriteLine("VALID TEST");
+                }
+                else
+                {
+                    Console.WriteLine("INVALID TEST");
+                }
+            }
+        }
+        static bool IsSimpleNumber(string input)
+        {
+            if (!Int32.TryParse(input, out int n))
+            {
+                throw new ArgumentException("Должно быть введено число!");
+            }
+            else if (n <= 0)
+            {
+                throw new ArgumentException("Число должно быть положительное (больше 0)!");
+            }
+            int d = 0;
+            int i = 2;
+            while (i < n)
+            {
+                if (n % i == 0)
+                {
+                    d++;
+                }
+                i++;
+            }
+            if (d == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
