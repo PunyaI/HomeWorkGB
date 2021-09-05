a5d5fa395b97f955421f07cfc1d11ca85c9a5675
Ilya <ronny0000@yandex.ru>
2021-09-05 23:03:14 +0300

    Итоговый вариант
    Генерация внутри теста, но оставил закоменченным вариант с генерацией снаружи, если нужен будет всё таки он

--- Program.cs
+++ Program.cs
@@ -64,6 +64,39 @@ namespace HomeWorkGB
             BenchmarkSwitcher.FromAssembly(typeof(BechmarkClass).Assembly).Run(args);
         }
     }
+    //public class PointGenerator
+    //{
+        
+    //    public static PointClass point1F = new PointClass() { X = 54.972313123123F, Y = 6F };
+    //    public static PointClass point2F = new PointClass() { X = 371.2322323232F, Y = 15.7F };
+    //    public static PointClass point3F = new PointClass() { X = 54.97F, Y = 16.2F };
+    //    public static PointClass point4F = new PointClass() { X = 371.232F, Y = 125.7F };
+    //    public static PointClass point5F = new PointClass() { X = 547.7231312368F, Y = 6F };
+    //    public static PointClass point6F = new PointClass() { X = 31.222312312F, Y = 15.7F };
+    //    public static PointClass point7F = new PointClass() { X = 4.7F, Y = 65.4F };
+    //    public static PointClass point8F = new PointClass() { X = 371.22F, Y = 1.72313125667F };
+    //    public static PointClass[] classpointsF = new PointClass[13] { point1F, point2F, point3F, point4F, point5F, point6F, point7F, point8F, point1F, point3F, point5F, point7F, point1F };
+        
+    //    public static PointStruct point1structF = new PointStruct() { X = 54.972313123123F, Y = 6F };
+    //    public static PointStruct point2structF = new PointStruct() { X = 371.2322323232F, Y = 15.7F };
+    //    public static PointStruct point3structF = new PointStruct() { X = 54.97F, Y = 16.2F };
+    //    public static PointStruct point4structF = new PointStruct() { X = 371.232F, Y = 125.7F };
+    //    public static PointStruct point5structF = new PointStruct() { X = 547.72313123687F, Y = 6F };
+    //    public static PointStruct point6structF = new PointStruct() { X = 31.222312312F, Y = 15.7F };
+    //    public static PointStruct point7structF = new PointStruct() { X = 4.7F, Y = 65.4F };
+    //    public static PointStruct point8structF = new PointStruct() { X = 371.22F, Y = 1.72313125667F };
+    //    public static PointStruct[] structpointsF = new PointStruct[13] { point1structF, point2structF, point3structF, point4structF, point5structF, point6structF, point7structF, point8structF, point1structF, point3structF, point5structF, point7structF, point1structF };
+        
+    //    public static PointStructDouble point1structD = new PointStructDouble() { X = 54.972313123123D, Y = 6D };
+    //    public static PointStructDouble point2structD = new PointStructDouble() { X = 371.2322323232D, Y = 15.7D };
+    //    public static PointStructDouble point3structD = new PointStructDouble() { X = 54.97D, Y = 16.2D };
+    //    public static PointStructDouble point4structD = new PointStructDouble() { X = 371.232D, Y = 125.7D };
+    //    public static PointStructDouble point5structD = new PointStructDouble() { X = 547.77231312368D, Y = 6D };
+    //    public static PointStructDouble point6structD = new PointStructDouble() { X = 31.222312312D, Y = 15.7D };
+    //    public static PointStructDouble point7structD = new PointStructDouble() { X = 4.7D, Y = 65.4D };
+    //    public static PointStructDouble point8structD = new PointStructDouble() { X = 371.22D, Y = 1.72313125667D };
+    //    public static PointStructDouble[] structpointsD = new PointStructDouble[13] { point1structD, point2structD, point3structD, point4structD, point5structD, point6structD, point7structD, point8structD, point1structD, point3structD, point5structD, point7structD, point1structD };
+    //}
     internal class Config : ManualConfig
     {
         [Obsolete]
@@ -78,67 +111,127 @@ namespace HomeWorkGB
     [Config(typeof(Config))]
     public class BechmarkClass
     {
-        PointClass point1F = new PointClass() { X = 5.32F, Y = 6.232F };
-        PointClass point2F = new PointClass() { X = 323.232F, Y = 153.232F };
-        PointClass point3F = new PointClass() { X = 52.32F, Y = 63.232F };
-        PointClass point4F = new PointClass() { X = 3234.23232F, Y = 1534.23232F };
-        PointClass point5F = new PointClass() { X = 4, Y = 10 };
-        PointClass point6F = new PointClass() { X = 15, Y = 2 };
-        PointStruct point1structF = new PointStruct() { X = 5.32F, Y = 6.232F };
-        PointStruct point2structF = new PointStruct() { X = 323.232F, Y = 153.232F };
-        PointStruct point3structF = new PointStruct() { X = 52.32F, Y = 63.232F };
-        PointStruct point4structF = new PointStruct() { X = 3234.23232F, Y = 1534.23232F };
-        PointStruct point5structF = new PointStruct() { X = 4, Y = 10 };
-        PointStruct point6structF = new PointStruct() { X = 15, Y = 2 };
-        PointStructDouble point1structD = new PointStructDouble() { X = 5.32D, Y = 6.232D };
-        PointStructDouble point2structD = new PointStructDouble() { X = 323.232D, Y = 153.232D };
-        PointStructDouble point3structD = new PointStructDouble() { X = 52.32D, Y = 63.232D };
-        PointStructDouble point4structD = new PointStructDouble() { X = 3234.23232D, Y = 1534.23232D };
-        PointStructDouble point5structD = new PointStructDouble() { X = 4, Y = 10 };
-        PointStructDouble point6structD = new PointStructDouble() { X = 15, Y = 2 };
 
         [Benchmark(Description = "Класс Point и тип данных Float")]
         public void TestClassFloat()
         {
-            PointDistance.PointDistanceClassFloat(point1F, point2F);
-            PointDistance.PointDistanceClassFloat(point2F, point3F);
-            PointDistance.PointDistanceClassFloat(point3F, point4F);
-            PointDistance.PointDistanceClassFloat(point4F, point5F);
-            PointDistance.PointDistanceClassFloat(point5F, point6F);
-            PointDistance.PointDistanceClassFloat(point6F, point1F);
+        PointClass point1F = new PointClass() { X = 54.972313123123F, Y = 6F };
+        PointClass point2F = new PointClass() { X = 371.2322323232F, Y = 15.7F };
+        PointClass point3F = new PointClass() { X = 54.97F, Y = 16.2F };
+        PointClass point4F = new PointClass() { X = 371.232F, Y = 125.7F };
+        PointClass point5F = new PointClass() { X = 547.7231312368F, Y = 6F };
+        PointClass point6F = new PointClass() { X = 31.222312312F, Y = 15.7F };
+        PointClass point7F = new PointClass() { X = 4.7F, Y = 65.4F };
+        PointClass point8F = new PointClass() { X = 371.22F, Y = 1.72313125667F };
+        PointClass[] classpointsF = new PointClass[13] { point1F, point2F, point3F, point4F, point5F, point6F, point7F, point8F, point1F, point3F, point5F, point7F, point1F };
+            for(int i = 0; i<12;i++)
+            {
+                //PointDistance.PointDistanceClassFloat(PointGenerator.classpointsF[i], PointGenerator.classpointsF[i + 1]);
+                PointDistance.PointDistanceClassFloat(classpointsF[i], classpointsF[i + 1]);
+            }
         }
 
         [Benchmark(Description = "Структура Point и тип данных Float")]
         public void TestStructFloat()
         {
-            PointDistance.PointDistanceStructFloat(point1structF, point2structF);
-            PointDistance.PointDistanceStructFloat(point2structF, point3structF);
-            PointDistance.PointDistanceStructFloat(point3structF, point4structF);
-            PointDistance.PointDistanceStructFloat(point4structF, point5structF);
-            PointDistance.PointDistanceStructFloat(point5structF, point6structF);
-            PointDistance.PointDistanceStructFloat(point6structF, point1structF);
+        PointStruct point1structF = new PointStruct() { X = 54.972313123123F, Y = 6F };
+        PointStruct point2structF = new PointStruct() { X = 371.2322323232F, Y = 15.7F };
+        PointStruct point3structF = new PointStruct() { X = 54.97F, Y = 16.2F };
+        PointStruct point4structF = new PointStruct() { X = 371.232F, Y = 125.7F };
+        PointStruct point5structF = new PointStruct() { X = 547.72313123687F, Y = 6F };
+        PointStruct point6structF = new PointStruct() { X = 31.222312312F, Y = 15.7F };
+        PointStruct point7structF = new PointStruct() { X = 4.7F, Y = 65.4F };
+        PointStruct point8structF = new PointStruct() { X = 371.22F, Y = 1.72313125667F };
+        PointStruct[] structpointsF = new PointStruct[13] { point1structF, point2structF, point3structF, point4structF, point5structF, point6structF, point7structF, point8structF, point1structF, point3structF, point5structF, point7structF, point1structF };
+            for (int i = 0; i < 12; i++)
+            {
+                //PointDistance.PointDistanceStructFloat(PointGenerator.structpointsF[i], PointGenerator.structpointsF[i+1]);
+                PointDistance.PointDistanceStructFloat(structpointsF[i], structpointsF[i + 1]);
+            }  
         }
 
         [Benchmark(Description = "Структура Point и тип данных Double")]
         public void TestStructDouble()
         {
-            PointDistance.PointDistanceStructDouble(point1structD, point2structD);
-            PointDistance.PointDistanceStructDouble(point2structD, point3structD);
-            PointDistance.PointDistanceStructDouble(point3structD, point4structD);
-            PointDistance.PointDistanceStructDouble(point4structD, point5structD);
-            PointDistance.PointDistanceStructDouble(point5structD, point6structD);
-            PointDistance.PointDistanceStructDouble(point6structD, point1structD);
+        PointStructDouble point1structD = new PointStructDouble() { X = 54.972313123123D, Y = 6D };
+        PointStructDouble point2structD = new PointStructDouble() { X = 371.2322323232D, Y = 15.7D };
+        PointStructDouble point3structD = new PointStructDouble() { X = 54.97D, Y = 16.2D };
+        PointStructDouble point4structD = new PointStructDouble() { X = 371.232D, Y = 125.7D };
+        PointStructDouble point5structD = new PointStructDouble() { X = 547.77231312368D, Y = 6D };
+        PointStructDouble point6structD = new PointStructDouble() { X = 31.222312312D, Y = 15.7D };
+        PointStructDouble point7structD = new PointStructDouble() { X = 4.7D, Y = 65.4D };
+        PointStructDouble point8structD = new PointStructDouble() { X = 371.22D, Y = 1.72313125667D };
+        PointStructDouble[] structpointsD = new PointStructDouble[13] { point1structD, point2structD, point3structD, point4structD, point5structD, point6structD, point7structD, point8structD, point1structD, point3structD, point5structD, point7structD, point1structD };
+            for (int i = 0; i < 12; i++)
+            {
+                //PointDistance.PointDistanceStructDouble(PointGenerator.structpointsD[i], PointGenerator.structpointsD[i + 1]);
+                PointDistance.PointDistanceStructDouble(structpointsD[i], structpointsD[i + 1]);
+            }  
         }
 
         [Benchmark(Description = "Структура Point тип данных Float без Sqrt")]
         public void TestStructFloatNoRoot()
         {
-            PointDistance.PointDistanceStructFloatWithoutSqrt(point1structF, point2structF);
-            PointDistance.PointDistanceStructFloatWithoutSqrt(point2structF, point3structF);
-            PointDistance.PointDistanceStructFloatWithoutSqrt(point3structF, point4structF);
-            PointDistance.PointDistanceStructFloatWithoutSqrt(point4structF, point5structF);
-            PointDistance.PointDistanceStructFloatWithoutSqrt(point5structF, point6structF);
-            PointDistance.PointDistanceStructFloatWithoutSqrt(point6structF, point1structF);
+            PointStruct point1structF = new PointStruct() { X = 54.972313123123F, Y = 6F };
+            PointStruct point2structF = new PointStruct() { X = 371.2322323232F, Y = 15.7F };
+            PointStruct point3structF = new PointStruct() { X = 54.97F, Y = 16.2F };
+            PointStruct point4structF = new PointStruct() { X = 371.232F, Y = 125.7F };
+            PointStruct point5structF = new PointStruct() { X = 547.72313123687F, Y = 6F };
+            PointStruct point6structF = new PointStruct() { X = 31.222312312F, Y = 15.7F };
+            PointStruct point7structF = new PointStruct() { X = 4.7F, Y = 65.4F };
+            PointStruct point8structF = new PointStruct() { X = 371.22F, Y = 1.72313125667F };
+            PointStruct[] structpointsF = new PointStruct[13] { point1structF, point2structF, point3structF, point4structF, point5structF, point6structF, point7structF, point8structF, point1structF, point3structF, point5structF, point7structF, point1structF };
+            for (int i = 0; i < 12; i++)
+            {
+                //PointDistance.PointDistanceStructFloatWithoutSqrt(PointGenerator.structpointsF[i], PointGenerator.structpointsF[i + 1]);
+                PointDistance.PointDistanceStructFloatWithoutSqrt(structpointsF[i], structpointsF[i + 1]);
+            }   
+        }
+    }
+    public class PointClass
+    {
+        public float X;
+        public float Y;
+    }
+    public struct PointStruct
+    {
+        public float X;
+        public float Y;
+    }
+
+    public struct PointStructDouble
+    {
+        public double X;
+        public double Y;
+    }
+
+
+    public class PointDistance
+    {
+        public static float PointDistanceClassFloat(PointClass pointone, PointClass pointtwo)
+        {
+            float x = pointone.X - pointtwo.X;
+            float y = pointone.Y - pointtwo.Y;
+            return (float)Math.Sqrt((x * x) + (y * y));
+        }
+
+        public static float PointDistanceStructFloat(PointStruct pointone, PointStruct pointtwo)
+        {
+            float x = pointone.X - pointtwo.X;
+            float y = pointone.Y - pointtwo.Y;
+            return (float)Math.Sqrt((x * x) + (y * y));
+        }
+        public static double PointDistanceStructDouble(PointStructDouble pointone, PointStructDouble pointtwo)
+        {
+            double x = pointone.X - pointtwo.X;
+            double y = pointone.Y - pointtwo.Y;
+            return Math.Sqrt((x * x) + (y * y));
+        }
+        public static float PointDistanceStructFloatWithoutSqrt(PointStruct pointone, PointStruct pointtwo)
+        {
+            float x = pointone.X - pointtwo.X;
+            float y = pointone.Y - pointtwo.Y;
+            return (x * x) + (y * y);
         }
 
     }
