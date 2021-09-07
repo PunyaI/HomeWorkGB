0555cf20a06ba064f0e01e2a9ab4d4f2ea345eff
Ilya <ronny0000@yandex.ru>
2021-09-07 22:06:10 +0300

    +Readme

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using BenchmarkDotNet.Exporters;
 using BenchmarkDotNet.Exporters.Csv;
 using BenchmarkDotNet.Running;
 using System;
-
+using System.Diagnostics;
 
 namespace HomeWorkGB
 {
@@ -51,17 +51,140 @@ namespace HomeWorkGB
                 expectedException = null
             };
 
-            Console.WriteLine("Тестирование №1");
-            TestCase.Test(testcase1);
-            Console.WriteLine();
-            Console.WriteLine("Тестирование №2");
-            TestCase.Test(testcase2);
-            Console.WriteLine();
-            Console.WriteLine("Тестирование №3");
-            TestCase.Test(testcase3);
-            Console.WriteLine();
+            //Console.WriteLine("Тестирование №1");
+            //TestCase.Test(testcase1);
+            //Console.WriteLine();
+            //Console.WriteLine("Тестирование №2");
+            //TestCase.Test(testcase2);
+            //Console.WriteLine();
+            //Console.WriteLine("Тестирование №3");
+            //TestCase.Test(testcase3);
+            //Console.WriteLine();
 
-            BenchmarkSwitcher.FromAssembly(typeof(BechmarkClass).Assembly).Run(args);
+
+
+
+
+            Console.WriteLine("Замер производительности при малом количестве вычислений (порядка 10)");
+            Console.WriteLine();
+            MyBenchmark(2);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Замер производительности при большом количестве вычислений (порядка 1 000 000 000)");
+            Console.WriteLine();
+            MyBenchmark(100000000);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Замер производительности при огромном количестве вычислений (порядка 10 000 000 000)"); //(порядка 100 000 000 000)
+            Console.WriteLine();
+            MyBenchmark(1000000000);
+
+
+
+            //BenchmarkSwitcher.FromAssembly(typeof(BechmarkClass).Assembly).Run(args);
+        }
+        static void MyBenchmark(float n)
+        {
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            PointClass point1F = new PointClass() { X = 54.972313123123F, Y = 6F };
+            PointClass point2F = new PointClass() { X = 371.2322323232F, Y = 15.7F };
+            PointClass point3F = new PointClass() { X = 54.97F, Y = 16.2F };
+            PointClass point4F = new PointClass() { X = 371.232F, Y = 125.7F };
+            PointClass point5F = new PointClass() { X = 547.7231312368F, Y = 6F };
+            PointClass point6F = new PointClass() { X = 31.222312312F, Y = 15.7F };
+            PointClass point7F = new PointClass() { X = 4.7F, Y = 65.4F };
+            PointClass point8F = new PointClass() { X = 371.22F, Y = 1.72313125667F };
+            PointClass[] classpointsF = new PointClass[13] { point1F, point2F, point3F, point4F, point5F, point6F, point7F, point8F, point1F, point3F, point5F, point7F, point1F };
+            for (int j = 0; j < n; j++)
+            {
+                for (int i = 0; i < 12; i++)
+                {
+                    PointDistance.PointDistanceClassFloat(classpointsF[i], classpointsF[i + 1]);
+                }
+            }
+            timer.Stop();
+            Console.WriteLine("Класс Point и тип данных Float. Выполнение заняло " + timer.ElapsedMilliseconds + " мс");
+            Console.WriteLine("Или " + timer.Elapsed + " нс");
+            Console.WriteLine();
+            timer.Reset();
+
+
+            timer.Start();
+            PointStruct point1structF = new PointStruct() { X = 54.972313123123F, Y = 6F };
+            PointStruct point2structF = new PointStruct() { X = 371.2322323232F, Y = 15.7F };
+            PointStruct point3structF = new PointStruct() { X = 54.97F, Y = 16.2F };
+            PointStruct point4structF = new PointStruct() { X = 371.232F, Y = 125.7F };
+            PointStruct point5structF = new PointStruct() { X = 547.72313123687F, Y = 6F };
+            PointStruct point6structF = new PointStruct() { X = 31.222312312F, Y = 15.7F };
+            PointStruct point7structF = new PointStruct() { X = 4.7F, Y = 65.4F };
+            PointStruct point8structF = new PointStruct() { X = 371.22F, Y = 1.72313125667F };
+            PointStruct[] structpointsF = new PointStruct[13] { point1structF, point2structF, point3structF, point4structF, point5structF, point6structF, point7structF, point8structF, point1structF, point3structF, point5structF, point7structF, point1structF };
+            for (int j = 0; j < n; j++)
+            {
+                for (int i = 0; i < 12; i++)
+                {
+                    PointDistance.PointDistanceStructFloat(structpointsF[i], structpointsF[i + 1]);
+                }
+            }
+
+            timer.Stop();
+            Console.WriteLine("Структура Point и тип данных Float. Выполнение заняло " + timer.ElapsedMilliseconds + " мс");
+            Console.WriteLine("Или " + timer.Elapsed + " нс");
+            Console.WriteLine();
+            timer.Reset();
+
+
+            timer.Start();
+            PointStructDouble point1structD = new PointStructDouble() { X = 54.972313123123D, Y = 6D };
+            PointStructDouble point2structD = new PointStructDouble() { X = 371.2322323232D, Y = 15.7D };
+            PointStructDouble point3structD = new PointStructDouble() { X = 54.97D, Y = 16.2D };
+            PointStructDouble point4structD = new PointStructDouble() { X = 371.232D, Y = 125.7D };
+            PointStructDouble point5structD = new PointStructDouble() { X = 547.77231312368D, Y = 6D };
+            PointStructDouble point6structD = new PointStructDouble() { X = 31.222312312D, Y = 15.7D };
+            PointStructDouble point7structD = new PointStructDouble() { X = 4.7D, Y = 65.4D };
+            PointStructDouble point8structD = new PointStructDouble() { X = 371.22D, Y = 1.72313125667D };
+            PointStructDouble[] structpointsD = new PointStructDouble[13] { point1structD, point2structD, point3structD, point4structD, point5structD, point6structD, point7structD, point8structD, point1structD, point3structD, point5structD, point7structD, point1structD };
+            for (int j = 0; j < n; j++)
+            {
+                for (int i = 0; i < 12; i++)
+                {
+                    PointDistance.PointDistanceStructDouble(structpointsD[i], structpointsD[i + 1]);
+                }
+            }
+
+            timer.Stop();
+            Console.WriteLine("Структура Point и тип данных Double. Выполнение заняло " + timer.ElapsedMilliseconds + " мс");
+            Console.WriteLine("Или " + timer.Elapsed + " нс");
+            Console.WriteLine();
+            timer.Reset();
+
+
+            timer.Start();
+            PointStruct point1struct = new PointStruct() { X = 54.972313123123F, Y = 6F };
+            PointStruct point2struct = new PointStruct() { X = 371.2322323232F, Y = 15.7F };
+            PointStruct point3struct = new PointStruct() { X = 54.97F, Y = 16.2F };
+            PointStruct point4struct = new PointStruct() { X = 371.232F, Y = 125.7F };
+            PointStruct point5struct = new PointStruct() { X = 547.72313123687F, Y = 6F };
+            PointStruct point6struct = new PointStruct() { X = 31.222312312F, Y = 15.7F };
+            PointStruct point7struct = new PointStruct() { X = 4.7F, Y = 65.4F };
+            PointStruct point8struct = new PointStruct() { X = 371.22F, Y = 1.72313125667F };
+            PointStruct[] structpoints = new PointStruct[13] { point1struct, point2struct, point3struct, point4struct, point5struct, point6struct, point7struct, point8struct, point1struct, point3struct, point5struct, point7struct, point1struct };
+            for (int j = 0; j < n; j++)
+            {
+                for (int i = 0; i < 12; i++)
+                {
+                    PointDistance.PointDistanceStructFloatWithoutSqrt(structpoints[i], structpoints[i + 1]);
+                }
+            }
+
+            timer.Stop();
+            Console.WriteLine("Структура Point и тип данных Float, без Sqrt. Выполнение заняло " + timer.ElapsedMilliseconds + " мс");
+            Console.WriteLine("Или " + timer.Elapsed + " нс");
+            Console.WriteLine();
+            timer.Reset();
         }
     }
     //public class PointGenerator
