c17935341f8cf78d42b17a0ff4934ebe3ddea7da
Ilya <ronny0000@yandex.ru>
2021-09-19 23:13:15 +0300

    Вариант 1.2
    Переделываю на меньшее количество бакетов

--- BucketSort.cs
+++ BucketSort.cs
@@ -6,11 +6,44 @@ using System.Threading.Tasks;
 
 namespace HomeWorkGB
 {
-    class BucketSort
+    public class BucketSort
     {
-        public static void DoBucketSort(int [] array)
+        public static void DoBucketSort(int[] array)
         {
-            
+           if (array == null || array.Length<2)
+            {
+                return;
+            }
+            int min = array[0];
+            int max = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                    min = array[i];
+                if (array[i] > max)
+                    max = array[i];
+            }
+            List<int>[] bucket = new List<int>[max - min + 1];
+            for (int i = 0; i < bucket.Length; i++)
+            {
+                bucket[i] = new List<int>();
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                bucket[array[i] - min].Add(array[i]);
+            }
+            int n = 0;
+            for (int i = 0; i < bucket.Length; i++)
+            {
+                if(bucket[i].Count > 0)
+                {
+                    for (int j = 0; j < bucket[i].Count; j++)
+                    {
+                        array[n] = bucket[i][j];
+                        n++;
+                    }
+                }
+            }
         }
     }
 }
