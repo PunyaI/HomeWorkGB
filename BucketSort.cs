4c8e484c0178f0e6cef057562ae9c92f21946ebb
Ilya <ronny0000@yandex.ru>
2021-09-19 22:20:51 +0300

    Start1

---
+++ BucketSort.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkGB
+{
+    class BucketSort
+    {
+        public static void DoBucketSort(int [] array)
+        {
+            
+        }
+    }
+}
