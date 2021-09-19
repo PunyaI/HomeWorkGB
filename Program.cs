600ecabe0ec1e20ea795ee6be6c2fa90886c8cbb
Ilya <ronny0000@yandex.ru>
2021-09-19 23:13:15 +0300

    Вариант 1
    Бакеты для каждого значения

--- Program.cs
+++ Program.cs
@@ -1,13 +1,32 @@
 ﻿using System;
 
+
 namespace HomeWorkGB
 {
     class Program
     {
-
+        static void PrintArray<T>(T []array)
+        {
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.ResetColor();
+            Console.WriteLine();
+        }
         static void Main(string[] args)
         {
-      
+            var rand = new Random();
+            int[] mas = new int[20];
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = rand.Next(0, 20);
+            }
+            PrintArray(mas);
+            BucketSort.DoBucketSort(mas);
+            PrintArray(mas);
         }
 
     }
