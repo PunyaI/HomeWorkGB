fa93798c0889122e4889be65887d3fa60185cb7a
Ilya <ronny0000@yandex.ru>
2021-09-29 21:34:35 +0300

    Вариант с удалением вершины
    Недоделанное удаление вершины

---
+++ Graph.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HomeWorkGB
+{
+    public interface IGraph
+    {
+        void AddVertex(int value); // добавить вершину
+        void AddEdge(int value1, int value2, int weight);
+        void RemoveVertex(int value); // удалить вершину по значению
+        Vertex GetVertexByValue(int value); //получить вершину графа по значению
+        void PrintGraph(); //вывести граф в консоль
+    }
+
+    public class Graph : IGraph
+    {
+        public List<Vertex> Vertexes = new List<Vertex>();
+        public Graph()
+        {
+        }
+        public Graph(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                AddVertex(i);
+            }
+        }
+        public void AddEdge(int startvalue, int endvalue, int weight)
+        {
+            var vertex1 = GetVertexByValue(startvalue);
+            var vertex2 = GetVertexByValue(endvalue);
+            if (vertex1 == null || vertex2 == null)
+                return;
+            var edge = new Edge() { Vert1 = vertex1, Vert2 = vertex2, Weight = weight };
+            vertex1.Edges.Add(edge);
+            vertex2.Edges.Add(edge);
+        }
+
+        public void AddVertex(int value)
+        {
+            var vertex = new Vertex() { Value = value };
+            Vertexes.Add(vertex);
+        }
+
+        public Vertex GetVertexByValue(int value)
+        {
+            foreach (var item in Vertexes)
+            {
+                if (item.Value == value)
+                    return item;
+            }
+            Vertex zero = new Vertex();
+            return zero;
+        }
+        public void PrintGraph()
+        {
+            foreach (var v in Vertexes)
+            {
+                Console.WriteLine("Вершина со значением - " + v.Value);
+                foreach (var edge in v.Edges)
+                {
+                    Console.WriteLine($"Имеет ребро между вершинами со значениями - {edge.Vert1.Value} и {edge.Vert2.Value}, вес ребра  - {edge.Weight}.");
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+
+        public void RemoveVertex(int value)
+        {
+            var vertex = GetVertexByValue(value);
+            for (int i = 0; i < vertex.Edges.Count; i++)
+            {
+                if (vertex.Edges[i].Vert1?.Value == value)
+                {
+                    for (int j = 0; j < vertex.Edges[i].Vert2.Edges.Count; j++)
+                    {
+                        if (vertex.Edges[i].Vert1?.Edges[j].Vert1?.Value == value || vertex.Edges[i].Vert1?.Edges[j].Vert2?.Value == value)
+                        {
+                            vertex.Edges[i].Vert1.Edges.RemoveAt(j);
+                        }  
+                    }
+                    vertex.Edges.RemoveAt(i);
+                }
+                if (vertex.Edges[i].Vert2?.Value == value)
+                {
+                    for (int j = 0; j < vertex.Edges[i].Vert1.Edges.Count; j++)
+                    {
+                        if (vertex.Edges[i].Vert2?.Edges[j].Vert1?.Value == value || vertex.Edges[i].Vert2?.Edges[j].Vert2?.Value == value)
+                        {
+                            vertex.Edges[i].Vert2?.Edges.RemoveAt(j);
+                            break;
+                        }
+                    }
+                    vertex.Edges.RemoveAt(i);
+                }
+
+            }
+            for (int i = 0; i < Vertexes.Count; i++)
+            {
+                if (vertex == Vertexes[i])
+                {
+                    Vertexes.RemoveAt(i);
+                }    
+            }
+        }
+    }
+    public class Vertex
+    {
+        public int Value { get; set; }
+        public List<Edge> Edges = new List<Edge>();
+    }
+    public class Edge
+    {
+        public int Weight { get; set; }
+        public Vertex Vert1 { get; set; }
+        public Vertex Vert2 { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,262 +5,128 @@ using System.Text;
 
 namespace HomeWorkGB
 {
-    
+
     class Program
     {
-        public interface IGraph
-        {
-            void AddItem(int value); // добавить вершину
-            void RemoveItem(int value); // удалить вершину по значению
-            Node GetNodeByValue(int value); //получить вершину графа по значению
-            //void PrintTree(Graph graph, int depth); //вывести граф в консоль
-        }
-        public class Node
-        {
-            public int Value { get; set; }
-            public List <Edge> Edges { get; set; }
-        }
-        public class Edge
-        {
-            public int Weight { get; set; }
-            public Node Node { get; set; }
-        }
-        public class Graph : IGraph
-        {
-            public Node root;
-            public void AddItem(int value)
-            {
-                throw new NotImplementedException();
-            }
 
-            public Node GetNodeByValue(int value)
-            {
-                throw new NotImplementedException();
-            }
-
-            public void RemoveItem(int value)
-            {
-                throw new NotImplementedException();
-            }
-        }
 
         static void Main(string[] args)
         {
-            var edgeAB = new Edge
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            //int N = 5;
-            //int[,] W = new int[N, N]; // Весовая матрица
-            //Load(W);
-            //Write(W);
-            //int[] active = new int[N]; // Состояния вершин (просмотрена или не просмотрена)
-            //int[] route = new int[N];
-            //int[] peak = new int[N];
-            //int i;
-            //int j;
-            //int min;
-            //int kMin = 0;
-
-            //for (i = 0; i < N; i++)
-            //{
-            //    active[i] = 1;
-            //    route[i] = W[0, i];
-            //    peak[i] = 0;
-            //}
-
-            //// Сразу помечаем, что вершина A(0) просмотрена,
-            //// с неё начинается маршрут
-            //active[0] = 0;
-
-            //for (i = 0; i < N - 1; i++)
-            //{
-            //    // Среди активных вершин
-            //    // ищем вершину с минимальным соответствующим значением в массиве R
-            //    // и проверяем, не лучше ли ехать через неё:
-            //    min = int.MaxValue;
-            //    for (j = 0; j < N; j++)
-            //        if (active[j] == 1 && route[j] < min)
-            //        {
-            //            min = route[j]; // Минимальный маршрут
-            //            kMin = j; // Номер вершины с минимальным маршрутом
-            //        }
-
-            //    active[kMin] = 0; // Просмотрели эту точку
-            //                      // Проверка маршрута через вершину kMin
-            //                      // Есть ли более короткий путь
-            //    for (j = 0; j < N; j++)
-            //        // Если текущий путь в вершину J (R[j]
-            //        // больше, чем путь из найденной вершины (R[kMin] +
-            //        // путь из этой вершины W[kMin][j], то
-            //        if (route[j] > route[kMin] + W[j, kMin] &&
-            //            W[j, kMin] != int.MaxValue && active[j] == 1)
-            //        {
-            //            // мы запоминаем новое расстояние
-            //            route[j] = route[kMin] + W[j, kMin];
-            //            // и запоминаем, что можем добраться туда более
-            //            // коротким путём в массиве P
-            //            peak[j] = kMin;
-            //        }
-            //}
-            //foreach (var item in peak)
-            //{
-            //    Console.Write($"{item} ");
-            //}
-            //Console.WriteLine();
-            //foreach (var item in route)
-            //{
-            //    Console.Write($"{item} ");
-            //}
-            //Console.WriteLine();
-            //while (i != 0)
-            //{
-            //    Console.Write($"{i} ");
-            //    i = peak[i];
-            //}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            //var tree = new Tree(20);
-            //for (int i = 0; i < 15; i++)
-            //{
-            //    tree.AddItem(i * 5);
-            //}
-        }
-        static void Load(int[,] mas)
-        {
-            int n = 0;//Int32.MaxValue;
-            mas[0, 0] = n;
-            mas[0, 1] = 2;
-            mas[0, 2] = 5;
-            mas[0, 3] = n;
-            mas[0, 4] = 7;
-            mas[1, 0] = 2;
-            mas[1, 1] = n;
-            mas[1, 2] = 1;
-            mas[1, 3] = n;
-            mas[1, 4] = n;
-            mas[2, 0] = 5;
-            mas[2, 1] = 1;
-            mas[2, 2] = n;
-            mas[2, 3] = 5;
-            mas[2, 4] = 1;
-            mas[3, 0] = n;
-            mas[3, 1] = n;
-            mas[3, 2] = 5;
-            mas[3, 3] = n;
-            mas[3, 4] = 3;
-            mas[4, 0] = 7;
-            mas[4, 1] = n;
-            mas[4, 2] = 1;
-            mas[4, 3] = 3;
-            mas[4, 4] = n;
-        }
-        static void Write(int[,] mas)
-        {
+            var graph = new Graph(7);
+            graph.AddEdge(1, 2, 1);
+            graph.AddEdge(2, 3, 3);
+            graph.AddEdge(2, 4, 2);
+            graph.AddEdge(2, 5, 1);
+            graph.AddEdge(3, 4, 1);
+            graph.AddEdge(3, 5, 4);
+            graph.AddEdge(4, 1, 2);
+            graph.AddEdge(4, 5, 2);
+            graph.AddEdge(5, 6, 3);
+            graph.AddEdge(7, 4, 1);
+            graph.PrintGraph();
+            int search = 6;
             Console.WriteLine();
-            for (int i = 0; i < mas.GetLength(0); i++)
-            {
-                for (int j = 0; j < mas.GetLength(1); j++)
-                {
-                    Console.Write(mas[i, j] + " ");
-                }
-                Console.WriteLine();
-            }
+            Console.WriteLine("                          Поиск в графе в ширину");
             Console.WriteLine();
+            var res = BFS(graph, search);
+            Console.WriteLine();
+            Console.WriteLine($"Результат поиска вершины со значением {search} - {res.Value}");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("                          Поиск в графе в глубину");
+            Console.WriteLine();
+            var res2 = DFS(graph, search);
+            Console.WriteLine();
+            Console.WriteLine($"Результат поиска вершины со значением {search} - {res2.Value}");
+
+            //Console.WriteLine();
+            //Console.WriteLine("Удаляем элемент со значением 6");
+            //graph.RemoveVertex(6);
+            //graph.PrintGraph();
         }
-        static TreeNode BFS(Tree tree, int search_value)
+
+        static Vertex BFS(Graph graph, int search_value)
         {
-            var res = new TreeNode();
-            var fail = new TreeNode();
-            var queue = new Queue<TreeNode>();
-            queue.Enqueue(tree.GetRoot()); //обходим дерево в ширину с помощью очереди в поисках нужного значения, если его нет, возвращаем пустой элемент
-            while (queue.Count != 0)
+            Queue<Vertex> q = new Queue<Vertex>();
+            Vertex vertex;
+            HashSet<Vertex> used = new HashSet<Vertex>();         //запоминаем какие вершины уже прошли
+            used.Add(graph.Vertexes[0]);
+            q.Enqueue(graph.Vertexes[0]);
+            Console.WriteLine("Добавляем в очередь первую вершину со значением " + graph.Vertexes[0].Value);
+            while(q.Count!=0)
             {
-                res = queue.Dequeue();
-                if (res?.Value == search_value)
+
+                vertex = q.Dequeue();
+                used.Add(vertex);
+                Console.WriteLine("Удаляем из очереди вершину со значением " + vertex.Value);
+                if (vertex.Value == search_value)
                 {
-                    return res;
+                    Console.WriteLine("Мы нашли искомую вершину со значением -  " + vertex.Value);
+                    return vertex;
                 }
-                if (res.LeftChild != null)
+                for (int i = 0; i < vertex.Edges.Count; i++)
                 {
-                    queue.Enqueue(res?.LeftChild);
-                }
-                if (res.RightChild != null)
-                {
-                    queue.Enqueue(res?.RightChild);
+                    if (!used.Contains(vertex.Edges[i].Vert1))
+                    {
+                        Console.WriteLine("Добавляем в очередь ещё не помеченную связанную вершину со значением -  " + vertex.Edges[i].Vert1.Value);
+                        used.Add(vertex.Edges[i].Vert1);
+                        q.Enqueue(vertex.Edges[i].Vert1);
+                    }
+                    if(!used.Contains(vertex.Edges[i].Vert2))
+                    {
+                        Console.WriteLine("Добавляем в очередь ещё не помеченную связанную вершину со значением -  " + vertex.Edges[i].Vert2.Value);
+                        used.Add(vertex.Edges[i].Vert2);
+                        q.Enqueue(vertex.Edges[i].Vert2);
+                    }  
                 }
             }
-            return fail;
+            vertex = null;
+            Console.WriteLine("Вершины с таким значением не найдено");
+            return vertex;
         }
-        static TreeNode DFS(Tree tree, int search_value)
+   
+        static Vertex DFS(Graph graph, int search_value)
         {
-            var res = new TreeNode();
-            var fail = new TreeNode();
-            var stack = new Stack<TreeNode>();
-            stack.Push(tree.GetRoot());                  //обходим дерево в глубину с помощью стека в поисках нужного значения, если его нет, возвращаем пустой элемент
-            while (stack.Count != 0)
+            Stack<Vertex> s = new Stack<Vertex>();
+            Vertex vertex;
+            HashSet<Vertex> used = new HashSet<Vertex>();         //запоминаем какие вершины уже прошли
+            used.Add(graph.Vertexes[0]);
+            s.Push(graph.Vertexes[0]);
+            Console.WriteLine("Добавляем в стек первую вершину со значением " + graph.Vertexes[0].Value);
+            while (s.Count!=0)
             {
-                res = stack.Pop();
-                if (res?.Value == search_value)
+                vertex = s.Pop();
+                Console.WriteLine("Вынимаем из стека вершину со значением " + vertex.Value);
+                used.Add(vertex);
+                if (vertex.Value == search_value)
                 {
-                    return res;
+                    Console.WriteLine("Мы нашли искомую вершину со значением -  " + vertex.Value);
+                    return vertex;
                 }
-                if (res.LeftChild != null)
+
+                for (int i = 0; i < vertex.Edges.Count; i++)
                 {
-                    stack.Push(res?.LeftChild);
-                }
-                if (res.RightChild != null)
-                {
-                    stack.Push(res?.RightChild);
+                    if (!used.Contains(vertex.Edges[i].Vert1))
+                    {
+                        Console.WriteLine("Добавляем в стек ещё не помеченную связанную вершину со значением -  " + vertex.Edges[i].Vert1.Value);
+                        used.Add(vertex.Edges[i].Vert1);
+                        s.Push(vertex.Edges[i].Vert1);
+                    }
+                    if (!used.Contains(vertex.Edges[i].Vert2))
+                    {
+                        Console.WriteLine("Добавляем в стек ещё не помеченную связанную вершину со значением -  " + vertex.Edges[i].Vert2.Value);
+                        used.Add(vertex.Edges[i].Vert2);
+                        s.Push(vertex.Edges[i].Vert2);
+                    }
                 }
             }
-            return fail;
+            Console.WriteLine("Вершины с таким значением не найдено");
+            vertex = null;
+            return vertex;
         }
+
     }
-        
+
 }
 
